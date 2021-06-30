f1371a1cb546f893351cf29a54867b9227784aa7
Rahul <rahulprk99@gmail.com>
2021-06-30 13:31:15 +0530

    adding POST method

--- src/handler/Requests.py
+++ src/handler/Requests.py
@@ -1,22 +1,34 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from flask import jsonify
+from flask import jsonify,request
 from bson.json_util import dumps
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/Actions"
-mongo = PyMongo(app)
+mongo1 = PyMongo(app, uri = 'mongodb://localhost:27017/Actions')
+mongo2 = PyMongo(app, uri = 'mongodb://localhost:27017/sampleInput')
 
 @app.route('/notes')
 def notes():
-    notes = mongo.db.Events.find()
+    notes = mongo1.db.Events.find()
     resp = dumps(notes)
     return resp
 
 @app.route('/getNotesByUserID/<id>')
 def getNotesByUserID(id):
-    patientResp = mongo.db.Events.find_one({"patientID":id})
+    patientResp = mongo1.db.Events.find_one({"patientID":id})
     resp = dumps(patientResp)
+    return resp
+
+@app.route('/setNotesByUserID/',methods=['POST'])
+def setNotesByUserID():
+    _json = request.json
+    patientID = _json['PatientID']
+    text = _json['Text']
+    actual = _json['Actual']
+
+    mongo2.db.Details.insert({"PatientID":patientID,"Text":text,"Actual":actual})
+    resp = jsonify("Added Successfully")
+    resp.status_code = 200
     return resp
 
 def main():
